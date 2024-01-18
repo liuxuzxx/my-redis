c3644d550b4fef0ac5c768a8da5c33c37f9f6144
liuxu <liuxu@wxchina.com>
2024-01-18 20:15:35 +0800

    1.完善执行的命令的效果

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use mini_redis::{Connection, Frame};
+use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -17,13 +17,26 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{Get, Set};
     let mut connection = Connection::new(socket);
 
-    let mut db: HashMap<String, String> = HashMap::new();
+    let mut db = HashMap::new();
 
     if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("Get :{:?}", frame);
-        let response = Frame::Error("unimplemented".to_string());
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
