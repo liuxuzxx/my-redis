6bce21c74fdeed3192be236cc45cc1cb78c92632
liuxu <liuxu@wxchina.com>
2024-01-19 19:31:21 +0800

    1.使用Mutux形式处理并发

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
+use bytes::Bytes;
 use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -8,27 +12,30 @@ async fn main() {
     let addr = String::from("127.0.0.1:7379");
     println!("Start mini redis server on {addr}");
     let listener = TcpListener::bind(addr).await.unwrap();
+
+    let db: Arc<Mutex<HashMap<String, Bytes>>> = Arc::new(Mutex::new(HashMap::new()));
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Bytes>>>) {
     use mini_redis::Command::{Get, Set};
     let mut connection = Connection::new(socket);
-
-    let mut db = HashMap::new();
 
     if let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
